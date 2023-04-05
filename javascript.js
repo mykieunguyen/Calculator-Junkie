840a1e209243e85788d6cd1872b6a50af6f801a8
mykieunguyen <mykieudonguyen@gmail.com>
2023-04-04 22:33:05 -0500

    Make operate function

--- javascript.js
+++ javascript.js
@@ -9,3 +9,23 @@ const multiply = (a,b) => a * b;
 //divide 
 const divide = (a, b) => a / b;
 
+// Calculator operation variable declartion 
+const firstNumber;
+const secondNumber;
+const operator; 
+
+// operate function 
+const operate = (a,b,operator) => {
+    if (operator === 'add') {
+        add(a,b);
+    }
+    else if (operator === 'subtract') {
+        subtract(a,b);
+    }
+    else if (operator === 'multiply') {
+        multiply(a,b);
+    }
+    else if (operator === 'divide') {
+        divide(a,b);
+    }
+}
