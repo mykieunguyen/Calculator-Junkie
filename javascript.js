9c643ddc0dfe8e0083b1561f8787233c41d2c7f2
mykieunguyen <mykieudonguyen@gmail.com>
2023-04-04 22:20:35 -0500

    Create add,subtract,multiply, and divide functions

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Calculator Functions 
+// Add
+function add (a, b) {
+    return a + b;
+}
+
+// Subtract 
+function subtract (a, b) {
+    return a - b;
+}
+
+// multiply 
+function multiply (a,b) {
+    return a * b;
+}
+
+//divide 
+function divide (a, b) {
+    return a/b;
+}
+
